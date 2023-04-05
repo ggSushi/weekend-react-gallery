08a86aa82140d5f1e392692e0aeaf0651455601d
Glonel Dimapilis <gdimapilis@aol.com>
2023-04-05 03:14:58 -0500

    Finally realized that I forgot to npm install react-router-dom, which is needed for both useParams and useHistory

--- src/components/GalleryList/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem.jsx
@@ -1,13 +1,36 @@
 import react from 'react';
+import {useState} from 'react';
+import {useParams} from 'react-router-dom';
 
 function GalleryItem({photo}) {
+
+  const { description } = useParams();
+
+  const [picStatus, setPicStatus] = useState(true);
+
+  const togglePic = () => {
+    setPicStatus(false);
+  }
+
 
   return(
     <>
     <div>
-      <img className="gallery-pics" src={photo.path}/>
-      {photo.description}
-      {photo.likes}
+      {
+        picStatus === true ? (
+          <img 
+            onClick={togglePic}
+            className="gallery-pics" 
+            src={photo.path}/>
+            // {photo.description}
+            // {photo.likes}
+          ) : (
+          <img 
+            onClick={togglePic}
+            className="gallery-pics" 
+            src={photo[description]}/>
+        )
+      }
     </div>
     </>
   )
